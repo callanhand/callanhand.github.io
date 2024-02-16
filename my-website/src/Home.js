d035d8c7449da98f976b4dacbc7360cb8bbfe8c6
handcg1 <handcg@dukes.jmu.edu>
2024-02-16 17:21:13 -0500

    start of animation

---
+++ my-website/src/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+import React, { useState } from 'react';
+
+
+
+function Home() {
+
+    return <div className='home'>This is the new page with a blue background.</div>;
+
+    
+  } 
+  
+  export default Home;
--- my-website/src/Intro.js
+++ my-website/src/Intro.js
@@ -1,42 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './Intro.css';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-
-function NewPage() {
-    return <div className="new-page">This is the new page with a blue background.</div>;
-  }
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
 function Modal({ isOpen, onClose }) {
     if (!isOpen) return null;
-  
+
     return (
-      <div className="modal-overlay">
-        <div className="modal">
-          <p class="error-modal">Invalid input. Please enter either Y or N.</p>
+        <div className="modal-overlay">
+            <div className="modal">
+                <p class="error-modal">Invalid input. Please enter either Y or N.</p>
+            </div>
         </div>
-      </div>
     );
-  }
+}
 
 
 function Intro() {
     const [userInput, setUserInput] = useState('');
     const [showModal, setShowModal] = useState(false);
-
-    //const inputRef = React.createRef(); // Create a ref object
-
-    // useEffect(() => {
-    //     // Focus the input element when the component mounts
-    //     //inputRef.current.focus();
-
-    //     // Add event listener for click on the document
-    //     document.addEventListener('click', handleClick);
-
-    //     // Cleanup function to remove event listener
-    //     return () => {
-    //         document.removeEventListener('click', handleClick);
-    //     };
-    // }, []);
+    const [fadeOut, setFadeOut] = useState(false);
+    const [fadeIn, setFadeIn] = useState(false);
+    const [show, setshow] = useState(false);
 
     const handleChange = (e) => {
         const inputValue = e.target.value.toUpperCase();
@@ -50,56 +35,85 @@ function Intro() {
             setTimeout(() => {
                 setShowModal(false);
                 setUserInput('');
-              }, 2500); // Close modal after 2.5 seconds
+            }, 2500); // Close modal after 2.5 seconds
 
             return;
         }
         if (userInput === 'Y') {
             console.log("I'm in");
-                  // Navigate to the new page
-            window.location.href = '/newPage';
+            setFadeOut(true);
+
+            // setTimeout(function () {
+            //     // $(".page__style").removeClass("animate_content");
+            //   }, 3200);
+            // Navigate to the new page
+
+            setTimeout(function () {
+                setFadeIn(true);
+              }, 1500);
+
+              setTimeout(function () {
+                setshow(true);
+              }, 3000);
+
+            // setTimeout(function () {
+            //     document.querySelector(".home").classList.add("fadeIn");
+            // }, 1500);
+
+            
+            // .page__description {
+            //     animation: fadeOutAndIn 3.9s forwards; /* Adjust the duration to match the total time */
+            //   }
+
+            //window.location.href = '/home';
             return;
         }
         // Reset the input field
         setUserInput('');
     };
 
-    const handleClick = () => {
-        // Focus the input element when the user clicks anywhere on the page
-        //inputRef.current.focus();
-    };
-
     const closeModal = () => {
         setShowModal(false);
-      };
+    };
 
     return (
-        <Router> 
-        <Routes>
-        <Route exact path="/newPage" element={<NewPage/>} />
-        <Route exact path="/" element={ 
-            <div class="container">
-                <p class="typed">$  Hi! My name is Callan Hand. </p>
-                <p class="typed">$  I'm an imaginative and passionate software engineer with
-                    a strong background </p>
-                <p class="typed"> in developing innovative solutions to complex technical puzzles. </p>
-                <p class="typed">$  Would you like to learn more?</p>
-                <p class="typed">$  Y / N </p>
+        <Router>
+            <Routes>
+                <Route exact path="/home" element={""} />
+                <Route exact path="/" element={
+                    <div> 
+                    <div class={`new-page ${fadeOut ? 'animate_content-in' : ''} ${fadeIn ? 'fade_in' : ''}`} > 
+                        <div class={`hidden ${show ? 'show' : ''}`}> 
+                            <Home> </Home>
+                        </div>
+
+                    </div>
+                    <div class={`container ${fadeOut ? 'animate_content' : ''}`}> 
+                        <p class="typed">$  Hi! My name is Callan Hand. </p>
+                        <p class="typed">$  I'm an imaginative and passionate software engineer with
+                            a strong background </p>
+                        <p class="typed"> in developing innovative solutions to complex technical puzzles. </p>
+                        <p class="typed">$  Would you like to learn more?</p>
+                        <p class="typed">$  Y / N </p>
+
+                        <form onSubmit={handleSubmit}>
+                            <label> $
+                                <input
+                                    type="text"
+                                    value={userInput}
+                                    onChange={handleChange}
+                                    autoFocus
+                                    onBlur={e => e.target.focus()}
+                                />
+                            </label>
+                        </form>
+                        <Modal isOpen={showModal} onClose={closeModal} />
+
+                    </div>
+                    </div>
+                } />
                 
-                <form onSubmit={handleSubmit}>
-                <label> $
-                    <input
-                        type="text"
-                        value={userInput}
-                        onChange={handleChange}
-                    />
-                    </label>
-                </form>
-               <Modal isOpen={showModal} onClose={closeModal} />
-                
-            </div>
-        }/>
-        </Routes>
+            </Routes>
 
         </Router>
     );
