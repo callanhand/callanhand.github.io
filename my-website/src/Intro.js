867afa793582c2e63274a1507a6bee28f56d072f
handcg1 <handcg@dukes.jmu.edu>
2024-04-10 17:46:08 -0400

    reworking home page

--- my-website/src/Intro.js
+++ my-website/src/Intro.js
@@ -3,6 +3,7 @@ import './Intro.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 
+
 function Modal({ isOpen, onClose }) {
     if (!isOpen) return null;
 
@@ -21,7 +22,7 @@ function Intro() {
     const [showModal, setShowModal] = useState(false);
     const [fadeOut, setFadeOut] = useState(false);
     const [fadeIn, setFadeIn] = useState(false);
-    const [show, setshow] = useState(false);
+    const [hide, setHide] = useState(true);
 
     const handleChange = (e) => {
         const inputValue = e.target.value.toUpperCase();
@@ -41,31 +42,18 @@ function Intro() {
         }
         if (userInput === 'Y') {
             console.log("I'm in");
-            setFadeOut(true);
+            // start animation 
+            setHide(true);
 
-            // setTimeout(function () {
-            //     // $(".page__style").removeClass("animate_content");
-            //   }, 3200);
-            // Navigate to the new page
+            // hide page after 
+            setTimeout(function () {
+                //setFadeIn(true);
+                setHide(true);
+            }, 1500);
 
             setTimeout(function () {
-                setFadeIn(true);
-              }, 1500);
-
-              setTimeout(function () {
-                setshow(true);
-              }, 3000);
-
-            // setTimeout(function () {
-            //     document.querySelector(".home").classList.add("fadeIn");
-            // }, 1500);
-
-            
-            // .page__description {
-            //     animation: fadeOutAndIn 3.9s forwards; /* Adjust the duration to match the total time */
-            //   }
-
-            //window.location.href = '/home';
+                //setHide
+            }, 5500);
             return;
         }
         // Reset the input field
@@ -77,45 +65,37 @@ function Intro() {
     };
 
     return (
-        <Router>
-            <Routes>
-                <Route exact path="/home" element={""} />
-                <Route exact path="/" element={
-                    <div> 
-                    <div class={`new-page ${fadeOut ? 'animate_content-in' : ''} ${fadeIn ? 'fade_in' : ''}`} > 
-                        <div class={`hidden ${show ? 'show' : ''}`}> 
-                            <Home> </Home>
-                        </div>
 
-                    </div>
-                    <div class={`container ${fadeOut ? 'animate_content' : ''}`}> 
-                        <p class="typed">$  Hi! My name is Callan Hand. </p>
-                        <p class="typed">$  I'm an imaginative and passionate software engineer with
-                            a strong background </p>
-                        <p class="typed"> in developing innovative solutions to complex technical puzzles. </p>
-                        <p class="typed">$  Would you like to learn more?</p>
-                        <p class="typed">$  Y / N </p>
+        <div>
+            {/* <div class={`${hide ? '' : 'hidden'} ${fadeOut ? 'animate_content' : ''}`}> <Home> </Home> </div> */}
+            
+            {/* <div class={`new-page ${fadeOut ? 'animate_content-in' : ''} ${fadeIn ? 'fade_in' : ''}`} /> */}
+            <Home> </Home>
 
-                        <form onSubmit={handleSubmit}>
-                            <label> $
-                                <input
-                                    type="text"
-                                    value={userInput}
-                                    onChange={handleChange}
-                                    autoFocus
-                                    onBlur={e => e.target.focus()}
-                                />
-                            </label>
-                        </form>
-                        <Modal isOpen={showModal} onClose={closeModal} />
+            {/* <div class='container'>
+                <p class={`typed ${hide ? 'disappear' : ''}`}>$  Hi! My name is Callan Hand. </p>
+                <p class="typed">$  I'm an imaginative and passionate software engineer with
+                    a strong background </p>
+                <p class="typed"> in developing innovative solutions to complex technical puzzles. </p>
+                <p class="typed">$  Would you like to learn more?</p>
+                <p class="typed">$  Y / N </p>
 
-                    </div>
-                    </div>
-                } />
-                
-            </Routes>
 
-        </Router>
+                <form onSubmit={handleSubmit}>
+                    <label> $
+                        <input
+                            type="text"
+                            value={userInput}
+                            onChange={handleChange}
+                            autoFocus
+                            onBlur={e => e.target.focus()}
+                        />
+                    </label>
+                </form>
+                <Modal isOpen={showModal} onClose={closeModal} />
+
+            </div> */}
+        </div>
     );
 }
 export default Intro;
